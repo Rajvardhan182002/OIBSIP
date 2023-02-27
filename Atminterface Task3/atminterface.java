d4756e79c38bac16a18a61c78d72ad9e4387a9b6
Rajvardhan182002 <124658564+Rajvardhan182002@users.noreply.github.com>
2023-02-27 18:44:56 +0530

    atminterface.java

---
+++ Atminterface Task3/atminterface.java
@@ -0,0 +1,17 @@
+package atminterface;
+import java.util.*;
+public class Atminterface {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter UserId:");
+		String UserId=sc.nextLine();
+		System.out.println("Enter UserPin:");
+		String UserPin=sc.nextLine();
+		AtmMethod obj=new AtmMethod(UserId,UserPin);
+		
+		obj.options();
+
+	}
+
+}
